004e36cc7f787add731ce814e94bc5b8ac5007cb
zer0xpoint <liyizhou@me.com>
2019-08-19 13:57:21 +0800

    update 1.4.15 two_sum_faster to O(N)

--- 1.Fundamentals/4.Analysis of Algorithm/15.py
+++ 1.Fundamentals/4.Analysis of Algorithm/15.py
@@ -1,15 +1,22 @@
 import random
 
 
-def two_sum_faster(list_a):
+def two_sum(list_a):
+    list_a.sort()
     lo = 0
     hi = len(list_a) - 1
     count = 0
     while lo < hi:
         if list_a[lo] + list_a[hi] == 0:
-            lo += 1
-            hi -= 1
             count += 1
+            lo += 1
+            # while list_a[lo] + list_a[hi] == 0:
+            #     lo += 1
+            #     count += 1
+            hi -= 1
+            # while list_a[lo] + list_a[hi] == 0:
+            #     hi -= 1
+            #     count += 1
         elif list_a[lo] + list_a[hi] < 0:
             lo += 1
         else:
@@ -17,7 +24,8 @@ def two_sum_faster(list_a):
     return count
 
 
-def three_sum_faster(list_a):
+def three_sum(list_a):
+    list_a.sort()
     count = 0
     for i in range(len(list_a)):
         lo = i + 1
@@ -34,11 +42,30 @@ def three_sum_faster(list_a):
     return count
 
 
-a = [random.randint(-100, 100) for i in range(100)]
-a.sort()
-print(two_sum_faster(a))
-print(three_sum_faster(a))
+# 以上方法需要先对list进行排序 复杂度为O(logN)
 
-# 以上是logN解决方法 需要线性N方法
-# 故用空间换时间 用python自带的dict来完成查找
-# 因为dict是一个hash_table 故查找只需要O1的时间完成
+# 空间换时间 用python自带的dict来完成查找
+# 因为dict是一个hash_table 故查找复杂度为O(1)
+# 对一个长度为N的数组进行查找 复杂度O(N)
+
+def two_sum_faster(list_a):
+    dict_a = {}
+    count = 0
+    for i in range(len(list_a)):
+        if dict_a.__contains__(-list_a[i]) and i != dict_a.get(-list_a[i]):
+            # dict_a.pop(-list_a[i])
+            # print(list_a[i], dict_a.get(-list_a[i]), i)
+            # del dict_a[-list_a[i]]
+            count += 1
+        dict_a[list_a[i]] = i
+    return count
+
+
+def three_sum_faster(list_a):
+    dict_a = {}
+
+
+a = [random.randint(-100, 100) for i in range(100)]
+# print(two_sum(a))
+# print(three_sum(a))
+print(two_sum_faster(a))
