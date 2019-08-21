2d9f02d8645ca24558093c7080a029e25a912a8a
zer0xpoint <liyizhou@me.com>
2019-08-21 10:00:51 +0800

    update 1.4.24 replace building(list) with constant

--- 1.Fundamentals/4.Analysis of Algorithm/24.py
+++ 1.Fundamentals/4.Analysis of Algorithm/24.py
@@ -2,19 +2,23 @@
 # 楼共N层高 在F层或F以上的楼层扔鸡蛋会碎
 # 要求复杂度O(2lgF)
 
-N = 100
-F = 72
-building = [i for i in range(1, N + 1)]
-print(building)
+N = 2147483647
+F = 6
 
 
-def binary_search(lo_index, hi_index, F):
+# building = [i for i in range(1, N + 1)]
+# print(building)
+
+
+def binary_search(lo_index, hi_index, F, run_time_count):
     while lo_index < hi_index:
+        run_time_count += 1
         floor = lo_index + (hi_index - lo_index) // 2
-        if building[floor] == F:
+        if floor == F:
             # print(floor)
-            return floor
-        elif building[floor] > F:
+            print(run_time_count)
+            return 'Find'
+        elif floor > F:
             hi_index = floor
         else:
             lo_index = floor + 1
@@ -22,9 +26,8 @@ def binary_search(lo_index, hi_index, F):
 
 # 一般的二分查找法 复杂度为O(lgN)
 lo = 0
-hi = len(building)
-print(binary_search(lo, hi, F))
-
+hi = N
+print(binary_search(lo, hi, F,0))
 
 # i以2的倍数递增的方式访问N 复杂度为lgF + lgF
 # 循环判断 2 ** i 是否大于F 每次循环中i递增 大于F退出循环
@@ -33,8 +36,10 @@ print(binary_search(lo, hi, F))
 # 应注意数组越界问题 2**i不应大于总楼层数N
 
 i = 0
+run_time_count = 0
 while 2 ** i < F:
+    run_time_count += 1
     i += 1
 F_lo = 2 ** (i - 1)
 F_hi = min(2 ** i, N)  # 处理越界
-print(binary_search(F_lo, F_hi, F))
+print(binary_search(F_lo, F_hi, F,run_time_count))
