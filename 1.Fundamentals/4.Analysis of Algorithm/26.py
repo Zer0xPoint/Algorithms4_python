536f73c6f2c47da69bba11bdc9a725fa01da4669
zer0xpoint <liyizhou@me.com>
2019-08-23 21:39:42 +0800

    update 1.4.27

--- 1.Fundamentals/4.Analysis of Algorithm/26.py
+++ 1.Fundamentals/4.Analysis of Algorithm/26.py
@@ -1,1 +1,4 @@
 # 三点共线
+# a+b+c = 0时 点（a,a**3）（b,b**3）（c,c**3）共线
+# todo
+# https://alg4.ikesnowy.com/1-4-26/
---
+++ 1.Fundamentals/4.Analysis of Algorithm/27.py
@@ -0,0 +1,12 @@
+# 两个栈实现的队列
+# 实现的队列的所有操作均摊后为常数时间
+
+from algs4.Stack import Stack
+
+N = 10
+s1 = Stack()
+s2 = Stack()
+
+list_a = [i for i in range(N)]
+print(list_a)
+
