4c81ce6820ad8cdc75c61def769c773e92a404e7
zer0xpoint <liyizhou@me.com>
2019-08-24 00:25:50 +0800

    update 1.4.29

--- 1.Fundamentals/4.Analysis of Algorithm/27.py
+++ 1.Fundamentals/4.Analysis of Algorithm/27.py
@@ -5,7 +5,7 @@ from algs4.Stack import Stack
 import random
 
 
-class Deque_by_two_Stack:
+class Queue_by_two_Stacks:
     def __init__(self):
         self._list_two_stack = [Stack(), Stack()]
         # self.s1 = Stack()
@@ -46,12 +46,11 @@ list_a = [i for i in range(N)]
 print(list_a)
 # print(push_or_pop)
 
-ds = Deque_by_two_Stack()
+ds = Queue_by_two_Stacks()
 
 # for i in range(N):
 #     if push_or_pop[i]:
 #         ds.push()
-# print(Deque_by_two_Stack()._list_two_stack)
 ds.push(0)
 ds.push(1)
 ds.push(2)
--- 1.Fundamentals/4.Analysis of Algorithm/28.py
+++ 1.Fundamentals/4.Analysis of Algorithm/28.py
@@ -1,4 +1,38 @@
 # 一个队列实现的栈
 # 要求每次操作复杂度均为线性
 
+from algs4.MyQueue import MyQueue
 
+
+class Stack_by_Queue:
+    def __init__(self):
+        self.__queue = MyQueue()
+        self.__N = 0
+
+    def is_empty(self):
+        return self.__N == 0
+
+    def size(self):
+        return self.__N
+
+    def push(self, item):
+        self.__queue.enqueue(item)
+        self.__N += 1
+
+    def pop(self):
+        for i in range(self.__N - 1):
+            self.__queue.enqueue(self.__queue.dequeue())
+        self.__queue.dequeue()
+        self.__N -= 1
+        # print(self.__queue)
+
+
+sq = Stack_by_Queue()
+sq.push(1)
+sq.push(2)
+sq.push(3)
+sq.push(4)
+sq.pop()
+sq.push(5)
+sq.pop()
+sq.pop()
